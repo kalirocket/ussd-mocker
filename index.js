570c37e429e0d5c1e95bc2d3e501ea408d9a5dd3
Siaw Henry Nartey <henrysiaw159@gmail.com>
2025-03-07 13:56:45 +0000

    FEAT: Migrate project to Express 5, restructure files, and improve formatting
    - Upgraded project to Express 5 and updated route handling
    - Migrated to ES module syntax ('import/export')
    - Fixed missing dependencies and updated package.json
    - Configured Prettier for code formatting and added formatting scripts
    - Added .gitignore to exclude node_modules and removed it from Git
    - Resolved view engine issues and ensured compatibility with .pug files
    - General code cleanup and optimizations

--- index.js
+++ index.js
@@ -1,27 +1,34 @@
-var express = require('express')
-  , util = require('util')
-  , bodyParser = require('body-parser')
-  , cookieParser = require('cookie-parser')
-  , route = require('./route')
-  ;
+import express from "express";
+import util from "util";
+import bodyParser from "body-parser";
+import cookieParser from "cookie-parser";
+import route from "./route.js";
+import dotenv from "dotenv";
 
-var app = express();
+dotenv.config();
+const app = express();
 
-app.set('port', 8773);
-app.set('domain', '127.0.0.1');
-app.set('view engine', 'jade');
-app.set('views', 'app/views');
+const PORT = process.env.PORT || 8773;
+const DOMAIN = process.env.DOMAIN || "127.0.0.1";
+
+app.set("view engine", "pug");
+app.set("views", "app/views"); // Keep this if needed
 
 app.use(cookieParser());
-app.use(bodyParser.urlencoded({extended: true}));
+app.use(bodyParser.urlencoded({ extended: true }));
 app.use(bodyParser.json());
 
-route(app);
+route(app); // Ensure `route.js` uses ESM
 
-app.use(express.static('public'));
+app.use(express.static("public"));
 
-app.listen(app.get('port'), app.get('domain'), function () {
-  console.log(util.format('\nPoint your browser to http://%s:%d to start using SMSGH USSD Mocker.\n'
-    + 'NOTE: Closing this window will stop the web application\'s server.'
-    , app.get('domain'), app.get('port')));
+app.listen(PORT, DOMAIN, () => {
+  console.log(
+    util.format(
+      "\nPoint your browser to http://%s:%d to start using SMSGH USSD Mocker.\n" +
+        "NOTE: Closing this window will stop the web application's server.",
+      DOMAIN,
+      PORT
+    )
+  );
 });
